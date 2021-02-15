5704dc696c133acf66f378b1eaf0467da8ace8e8
sean galloway <sean.galloway@outlook.com>
2021-02-14 16:27:42 -0800

    got the word cloud working

--- app.py
+++ app.py
@@ -12,21 +12,25 @@ client = pymongo.MongoClient(conn)
 db = client.movies_db
 movies = db.movie_table
 
+###############################################################################################
 # Base route on bring up
 @app.route("/")
 def index():
     return render_template("home.html")
 
+###############################################################################################
 # maps route
 @app.route("/maps")
 def maps():
     return render_template("maps.html")
 
+###############################################################################################
 # scatter route
 @app.route("/scatter")
 def scatter():
     return render_template("scatter.html")
 
+###############################################################################################
 # word cloud route
 @app.route("/wordcloud", methods=['POST', 'GET'])
 def wordcloud():
@@ -44,6 +48,10 @@ def wordcloud():
                 }
         genre_match = movies.find(query)
         print("---------------------> Count Match: " + str(genre_match.count(True)))
+        text_string = ""
+        for cursor in genre_match:
+            text_string += cursor.get("keywords")
+
     return render_template("cloud.html", text_string = text_string)
 
 if __name__ == "__main__":
