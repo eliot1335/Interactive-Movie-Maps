1f33ea835d517e40c6b315dfcc9b69cc04c9cd6a
Cora Micsunescu <csmicsun@icloud.com>
2021-02-16 10:09:56 -0800

    js changes

--- static/js/world.js
+++ static/js/world.js
@@ -3,13 +3,11 @@ var myMap = L.map("map",{
     zoom:2
 });
 
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "light-v10",
   accessToken: API_KEY
-}).addTo(myMap);
+});
 
 var production_countries =[];
