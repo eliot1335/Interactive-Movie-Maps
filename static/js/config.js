3ef855493d13cc3bff530d19a57236bf96d2bf49
Cora Micsunescu <csmicsun@icloud.com>
2021-02-16 19:49:59 -0800

    change mapbox API Key

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,1 @@
-const API_KEY = "pk.eyJ1IjoiY3NtaWNzdW4iLCJhIjoiY2trcmI0MTJoMTk4YjJvbnZnbm9zYjhqYSJ9.d3eJDz28fzi8gE0hHmd6Hg"
+const API_KEY = "pk.eyJ1IjoiY3NtaWNzdW4iLCJhIjoiY2tsOHY4bTdjMDFyMzJ3cGZ4ejJjd2ZlaiJ9.CcEXO5a02LLK-y3F5qTXhw";
