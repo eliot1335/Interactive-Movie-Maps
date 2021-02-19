5ebb54f31458b0c91a60565902f680e383ba33b7
Eliot Chern <eliotchern@gmail.com>
2021-02-19 10:52:31 -0800

    adding some features to scatter plot

--- static/js/scatter.js
+++ static/js/scatter.js
@@ -1,26 +1,29 @@
 // Setup the parameters
 var svgWidth = 800;
-var svgHeight = 600;
+var svgHeight = 500;
 
 var margin = {
     top: 40,
     right: 40,
     bottom: 80,
-    left: 90
+    left: 250
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
-console.log("this is line 14");
+
 var svg = d3.select("#scatter")
             .append("svg")
             .attr("width", svgWidth)
             .attr("height", svgHeight);
-            console.log("this is line 19");
+
 var chartGroup = svg
             .append("g")
             .attr("transform", `translate(${margin.left}, ${margin.top})`);
             // .attr("pointer-events", "stroke")
+
+
+// var colorScale = d3.scale.category20()
 
 // Import Data
 d3.json("/metadata/scatter_plot").then(function (movieData) {
@@ -36,13 +39,11 @@ d3.json("/metadata/scatter_plot").then(function (movieData) {
     var xLinearScale = d3.scaleLinear()
         .domain([d3.min(movieData, d => d.budget),
                  d3.max(movieData, d => d.budget)])
-        // .domain([0, 500000000])
         .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
         .domain([d3.min(movieData, d => d.revenue),
                  d3.max(movieData, d => d.revenue)])
-        // .domain([0, 500000000])
         .range([height, 0]);
 
     // Create axis functions
@@ -60,7 +61,7 @@ d3.json("/metadata/scatter_plot").then(function (movieData) {
     // Create axes labels
     chartGroup.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 -margin.left + 50)
+        .attr("y", 0 -margin.left + 150)
         .attr("x", 0 - (height / 2) -60)
         .attr("class", "axisText")
         .text("Revenue");
@@ -77,9 +78,31 @@ d3.json("/metadata/scatter_plot").then(function (movieData) {
         .append("circle")
         .attr("cx", d => xLinearScale(d.budget))
         .attr("cy", d => yLinearScale(d.revenue))
-        .style("r", 10)
-        .style("fill", "teal")
-        .style("opacity", "0.6");
+        .attr("r", 10)
+        .attr("stroke", "black")
+        .attr("stroke-width", 1)
+        .attr("fill", "teal")
+        // .attr('fill',function (d,i) { return colorScale(i) })
+        .attr("opacity", "0.6")
+
+
+        .on("mouseover", function () {
+            d3.select(this)
+            .transition()
+            // .duration(500)
+            .attr("r", 20)
+            .attr("stroke-width", 3)
+        })
+        .on("mouseout", function () {
+            d3.select(this)
+            .transition()
+            // .duration(500)
+            .attr("r", 10)
+            .attr("stroke-width", 1)
+        })
+        .append("title") // Tooltip
+            .text(function (d) { return d.title });
+
 
     // Create circle labels
     // chartGroup.selectAll()
