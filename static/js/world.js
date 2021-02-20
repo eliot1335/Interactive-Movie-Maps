69ffe9edc8d40456c6d69fa274ef663f7bb7e66a
Cora Micsunescu <csmicsun@icloud.com>
2021-02-20 14:26:56 -0800

    rerun etl

--- static/js/world.js
+++ static/js/world.js
@@ -8,9 +8,14 @@ var myMap = L.map("map", {
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satellite-v9',
-    tileSize: 256,
+    id: 'mapbox/satellite-v11',
+    tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(myMap);
+
+
+var productionCountries= [{
+  name: 
+}]
 
