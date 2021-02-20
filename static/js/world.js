36d94c2f5c5c3f633db8a12a60317b8542b79c72
Cora Micsunescu <csmicsun@icloud.com>
2021-02-20 13:25:28 -0800

    change name of project

--- static/js/world.js
+++ static/js/world.js
@@ -9,11 +9,8 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/satellite-v9',
-    tileSize: 512,
+    tileSize: 256,
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(myMap);
 
-d3.json("/metadata/world_map").then(function (createMarkers){
-   
-})
